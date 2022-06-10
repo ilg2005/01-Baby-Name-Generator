6623ee21f53898d285644a5b3b1eb148569df873
Igor <ilg2005@mail.ru>
2022-06-10 20:26:21 +0300

    Initial commit

---
+++ app.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+    <h1>Baby Name Generator</h1>
+    <p>Choose your options and click the "Find Names" button below</p>
+    <div class="options-container">
+      <div class="option-container">
+        <h4>1) Choose a gender</h4>
+        <div class="option-buttons">
+          <button
+              class="option option-left"
+          >
+            Boy
+          </button>
+          <button
+              class="option"
+          >
+            Unisex
+          </button>
+          <button
+              class="option option-right"
+          >
+            Girl
+          </button>
+        </div>
+      </div>
+      <div class="option-container">
+        <h4>2) Choose the name's popularity</h4>
+        <div class="option-buttons">
+          <button
+              class="option option-left"
+          >
+            Trendy
+          </button>
+          <button
+              class="option option-right"
+          >
+            Unique
+          </button>
+        </div>
+      </div>
+      <div class="option-container">
+        <h4>3) Choose name's length</h4>
+        <div class="option-buttons">
+          <button
+              class="option option-left"
+          >
+            Long
+          </button>
+          <button
+              class="option"
+          >
+            All
+          </button>
+          <button
+              class="option option-right"
+          >
+            Short
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
